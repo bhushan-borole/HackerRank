4046771123ba56a64bc6a263b86ed825ef7a685e
bhushan-borole <37565807+bhushan-borole@users.noreply.github.com>
2018-10-09 22:48:48 +0530

    30 days of code updated

---
+++ 30 Days of Code/Abstract Classes.java
@@ -0,0 +1,18 @@
+// https://www.hackerrank.com/challenges/30-abstract-classes/problem
+
+class MyBook extends Book{
+    String title;
+    String author;
+    int price;
+    public MyBook(String title, String author, int price){
+        super(title, author);
+        this.title = title;
+        this.author = author;
+        this.price = price;
+    }
+    public void display(){
+        System.out.println("Title: " + title);
+        System.out.println("Author: " + author);
+        System.out.println("Price: " + price);
+    }
+} 
