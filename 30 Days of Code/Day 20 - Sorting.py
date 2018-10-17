0304d23e0449e2d3fe81f660a7eaf19e4a2e7500
bhushan-borole <37565807+bhushan-borole@users.noreply.github.com>
2018-10-17 14:09:17 +0530

    30 day of code updated

---
+++ 30 Days of Code/Day 20 - Sorting.py
@@ -0,0 +1,15 @@
+# https://www.hackerrank.com/challenges/30-sorting/problem
+
+n = int(input().strip())
+a = list(map(int, input().strip().split(' ')))
+numberOfSwaps = 0
+for i in range(len(a) - 1):
+    for j in range(len(a) - 1):
+        if a[j] > a[j+1]:
+            a[j], a[j+1] = a[j+1], a[j]
+            numberOfSwaps  += 1
+    if numberOfSwaps == 0:
+        break
+print('Array is sorted in '+ str(numberOfSwaps) +' swaps.')
+print('First Element: ' + str(a[0]))
+print('Last Element: ' + str(a[-1]))
