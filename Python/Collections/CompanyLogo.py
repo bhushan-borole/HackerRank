9bfb2afc6c3e584660d898126cacc658678d8779
bhushan-borole <37565807+bhushan-borole@users.noreply.github.com>
2019-01-29 23:44:00 +0530

    company logo added

---
+++ Python/Collections/CompanyLogo.py
@@ -0,0 +1,7 @@
+# https://www.hackerrank.com/challenges/most-commons/problem
+
+from collections import Counter, OrderedDict
+
+class OrderedCounter(Counter, OrderedDict):
+    pass
+[print(*c) for c in OrderedCounter(sorted(input())).most_common(3)]
